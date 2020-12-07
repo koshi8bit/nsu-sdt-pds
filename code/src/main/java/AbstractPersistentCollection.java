dd31beba13adfc4021544657a62bd22739e50b53
deusestmortuus <kuznetsov1398@gmail.com>
2020-12-07 20:13:09 +0700

    Исправлен maxCount

--- code/src/main/java/AbstractPersistentCollection.java
+++ code/src/main/java/AbstractPersistentCollection.java
@@ -3,9 +3,9 @@ import java.util.List;
 public abstract class AbstractPersistentCollection<E> implements UndoRedo, List<E> {
     public int depth = 3;
     public int bit_dlya_rasc_ur = Node.bit_na_pu * depth;
-    public int mask = maxCount();
+    public int mask = (int) Math.pow(2, Node.bit_na_pu) - 1;
 
     private int maxCount() {
-        return (int) Math.pow(2, Node.bit_na_pu) - 1;
+        return (int) Math.pow(2, bit_dlya_rasc_ur);
     }
 }
