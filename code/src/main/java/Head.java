56d14e43bfb9844bab217a54684879c49b5b1d3b
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2020-12-07 22:07:07 +0700

    copy path begin

--- code/src/main/java/Head.java
+++ code/src/main/java/Head.java
@@ -4,13 +4,11 @@ public class Head<E> {
 
     public Head() {
         this.root = new Node<>();
-        root.parent = null;
         this.count = 0;
     }
 
     public Head(Head<E> prevHead) {
         this.root = new Node<>(prevHead.root);
-        this.root.parent = null;
         this.count = prevHead.count + 1;
     }
 }
--- code/src/main/java/Node.java
+++ code/src/main/java/Node.java
@@ -5,7 +5,7 @@ public class Node<E> {
     public static int bit_na_pu = 1;
     public static int width;
     public List<E> data;
-    public List<Node<E>> children = new ArrayList<>();  // пусть изначально равно null
+    public List<Node<E>> children = new ArrayList<>(); // TODO make as data: null as default
 
     static {
         width = (int) Math.pow(2, bit_na_pu);
@@ -17,13 +17,17 @@ public class Node<E> {
 
     public Node(Node<E> prevRoot) {
         //TODO check
-        if (prevRoot.children == null) // TODO
-        children.addAll(prevRoot.children);
+        if (prevRoot.children != null)
+            children.addAll(prevRoot.children);
+
+        if (prevRoot.data != null) {
+            data = new ArrayList<>();
+            data.addAll(prevRoot.data);
+        }
     }
 
     public void createChildren() {
         Node<E> node = new Node<E>();
-        node.parent = this;
 
         if (children.size() < width)
             children.add(node);
