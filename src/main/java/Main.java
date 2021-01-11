2c6e0c4d42329312435e404f50fbb6fc860cc812
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2021-01-12 02:41:12 +0700

    list resuming..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        arrayPresentation();
-//        listPresentation();
+        //arrayPresentation();
+        listPresentation();
 
 
     }
