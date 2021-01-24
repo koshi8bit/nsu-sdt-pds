92d8a99fb8e29f1e9344358be3fd3efdcfed06cf
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2021-01-24 17:49:46 +0700

    before exam

--- src/test/java/PersistentHashMapTest.java
+++ src/test/java/PersistentHashMapTest.java
@@ -230,7 +230,7 @@ public class PersistentHashMapTest {
         PersistentHashMap<String, Integer> child2 = new PersistentHashMap<>();
         PersistentHashMap<String, Integer> child3 = new PersistentHashMap<>();
 
-        parent.get("child1").put("One", 1);
+        //parent.get("child1").put("One", 1);
 
         parent.put("child1", child1);
         parent.put("child2", child2);
